85b45e1f03a9dd8dfcd614bffb5ffb5a1b0c99fc
Marvellous <marvellouschidindu@outlook.com>
2023-11-12 04:37:18 -0800

    me

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -21,23 +21,23 @@ char **strtow(char *str)
 	i = flag = len = 0;
 	while (str[i])
 	{
-	if (flag == 0 && str[i] != ' ')
-	flag = 1;
-	if (i > 0 && str[i] == ' ' && str[i - 1] != ' ')
-	{
-	flag = 0;
-	len++;
-	}
-	i++;
+		if (flag == 0 && str[i] != ' ')
+			flag = 1;
+		if (i > 0 && str[i] == ' ' && str[i - 1] != ' ')
+		{
+			flag = 0;
+			len++;
+		}
+		i++;
 	}
 
 	len += flag == 1 ? 1 : 0;
 	if (len == 0)
-	return (NULL);
+		return (NULL);
 
 	words = (char **)malloc(sizeof(char *) * (len + 1));
 	if (words == NULL)
-	return (NULL);
+		return (NULL);
 
 	util(words, str);
 	words[len] = NULL;
@@ -56,24 +56,24 @@ void util(char **words, char *str)
 	i = j = flag = 0;
 	while (str[i])
 	{
-	if (flag == 0 && str[i] != ' ')
-	{
-	start = i;
-	flag = 1;
-	}
+		if (flag == 0 && str[i] != ' ')
+		{
+			start = i;
+			flag = 1;
+		}
 
-	if (i > 0 && str[i] == ' ' && str[i - 1] != ' ')
-	{
-	create_word(words, str, start, i, j);
-	j++;
-	flag = 0;
-	}
+		if (i > 0 && str[i] == ' ' && str[i - 1] != ' ')
+		{
+			create_word(words, str, start, i, j);
+			j++;
+			flag = 0;
+		}
 
-	i++;
+		i++;
 	}
 
 	if (flag == 1)
-	create_word(words, str, start, i, j);
+		create_word(words, str, start, i, j);
 }
 
 /**
@@ -91,7 +91,7 @@ void create_word(char **words, char *str, int start, int end, int index)
 	i = end - start;
 	words[index] = (char *)malloc(sizeof(char) * (i + 1));
 
-	for (j = 0; start < end; start++, j++;
-	words[index][j] = str[start];
+	for (j = 0; start < end; start++, j++)
+		words[index][j] = str[start];
 	words[index][j] = '\0';
 }
