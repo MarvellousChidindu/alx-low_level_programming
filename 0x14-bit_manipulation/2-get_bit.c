89397952041d6919f4d2c8d7c3824bc48b2aec21
Marvellous <marvellouschidindu@outlook.com>
2023-11-23 07:04:26 -0800

    get bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+ * get_bit - prog returns the value of a bit at a given index.
+ * @n: number to check bits in
+ * @index: index at which to check bit
+ *
+ * Return: value of the bit, or -1 if there is an error
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int b, c;
+
+	if (index > (sizeof(unsigned long int) * 8 - 1))
+		return (-1);
+	b = 1 << index;
+	c = n & b;
+	if (c == b)
+		return (1);
+	return (0);
+}
